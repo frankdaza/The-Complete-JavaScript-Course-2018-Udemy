47fdc090060ace37ae9faafc12709de3777dfa0d
Frank Edward Daza Gonzalez <frank.daza2@gmail.com>
2018-05-21 23:08:29 -0500

    Variables and Data Types

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -1,1 +1,11 @@
-console.log('Hello world of Knarf!');
+var name = "Frank Edward";
+console.log(name);
+
+var lastName = "Daza González";
+console.log(lastName);
+
+var age = 28;
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);
