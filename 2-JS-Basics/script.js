741f98a2cf8c349eafd48b08582a19323acea8a3
Frank Edward Daza Gonzalez <frank.daza2@gmail.com>
2018-05-21 23:26:04 -0500

    Variable Mutation and Type Coercion

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -1,3 +1,5 @@
+
+/**
 var name = "Frank Edward";
 console.log(name);
 
@@ -9,3 +11,27 @@ console.log(age);
 
 var fullAge = true;
 console.log(fullAge);
+ */
+
+
+ // Lecture: variables 2
+var name = "John";
+var age = 26;
+
+// console.log(name + age);
+// console.log(age + age);
+
+var job, isMarried;
+// console.log(job);
+
+job = "Teacher";
+isMarried = false;
+console.log(name + " is a " + age + " years old " + job + " Is he married? " + isMarried);
+
+age = "thirty six";
+job = "driver";
+console.log(name + " is a " + age + " years old " + job + " Is he married? " + isMarried);
+
+var lastName = prompt("What is the last name?");
+console.log("The lastname is: " + lastName);
+alert("The lastname is: " + lastName);
