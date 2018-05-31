b38de7ac9f933178a9e3ef91df0aa9be389f49e6
Frank Edward Daza Gonzalez <frank.daza2@gmail.com>
2018-05-30 23:24:00 -0500

    Updating Scores and Changing the Active Player

--- 4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/app.js
@@ -9,13 +9,56 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
-dice = Math.floor(Math.random() * 6) + 1;
 
-document.querySelector("#current-" + activePlayer).textContent = dice;
+var diceDOM = document.getElementById("diceIMG");
+diceDOM.style.display = "none";
 
-var x = document.querySelector("#current-" + activePlayer).textContent;
+var score0 = document.getElementById("score-0");
+score0.textContent = "0"
+
+var score1 = document.getElementById("score-1");
+score1.textContent = "0"
+
+var current0 = document.getElementById("current-0");
+current0.textContent = "0"
+
+var current1 = document.getElementById("current-1");
+current1.textContent = "0"
+
+var panel0 = document.getElementById("panel0");
+var panel1 = document.getElementById("panel1");
+
+document.getElementById("btnRoll").addEventListener("click", function() {
+    // 1. Random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    // 2. Display the result
+    diceDOM.style.display = "block";
+    diceDOM.src = "dice-" + dice + ".png";
+
+    // 3. Update the round score if the rolled number was not a 1
+    if (dice !== 1) {
+        // Add score        
+        roundScore += dice;
+
+        if (activePlayer === 0) {
+            current0.textContent = roundScore;
+        } else {
+            current1.textContent = roundScore;
+        }
+    } else {
+        // Next player
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        roundScore = 0;
+        current0.textContent = "0";
+        current1.textContent = "0";
+        diceDOM.style.display = "none";
+        panel0.classList.toggle("active")
+        panel1.classList.toggle("active")
+    }
+});
