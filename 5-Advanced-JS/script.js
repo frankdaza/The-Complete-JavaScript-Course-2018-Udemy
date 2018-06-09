72713ebc6a55537387522935ba2009dd0bd00d51
Frank Edward Daza Gonzalez <frank.daza2@gmail.com>
2018-06-08 20:56:41 -0500

    Primitives vs. Objects

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -25,7 +25,7 @@ Person.prototype.hello = function() {
 
 var john = new Person("John Wick", 1978, "Vigilante");
 var mark = new Person("Mark Wilson", 1960, "Teacher");
- */
+
 
 // Object.create
 var personProto = {
@@ -38,3 +38,58 @@ var frank = Object.create(personProto);
 frank.name = "Frank Daza"
 frank.job = "Systems engineer"
 frank.yearOfBirth = 1989;
+ */
+
+// Primitives vs Objects
+
+// Primitives
+var a = 23;
+var b = a;
+a = 46;
+console.log("a = " + a);
+console.log("b = " + b);
+
+// Objects
+var obj1 = {
+  name: "Frank Daza",
+  age: 28
+};
+
+var obj2 = obj1;
+obj1.age = 15;
+console.log("obj1.age = " + obj1.age);
+console.log("obj2.age = " + obj2.age);
+
+// Functions
+var age = 24;
+var obj = {
+  name: "Chango Man",
+  city: "Santiago de Chile"
+};
+
+function change(a, b) {
+  a = 30;
+  b.city = "San Francisco";
+};
+
+console.log("age = " + age);
+console.log("b.city = " + b.city);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
