c49fda43ca25d2c789b8da6d41e0f8c4865ef460
Frank Edward Daza González <frank.daza2@gmail.com>
2018-08-21 16:30:58 -0500

    Variable declarations with let and const

---
+++ 7-ES6/script.js
@@ -0,0 +1,54 @@
+// Lecture: let and const
+
+/*
+
+// ES5
+var name5 = "John Snow";
+name5 = "Johan Gutierrez";
+
+// ES6
+//const name6 = "Frank Daza";
+let name6 = "Frank Daza";
+name6 = "Frank Gonzalez";
+
+*/
+
+function driversLicense5(passedTest) {
+    
+    console.log(firstName);
+    
+    if (passedTest) {
+        var firstName = "Frank";
+        var yearOfBirth = 1989;
+
+        console.log(firstName + " born in " + yearOfBirth
+         + ", is now officially allowed to drive a car. ES5.");
+    }
+}
+
+driversLicense5(true);
+
+function driversLicense6(passedTest) {
+
+    // console.log(firstName);
+
+    let firstName;
+    const yearOfBirth = 1989;
+
+    if (passedTest) {
+        firstName = "Frank";
+    }
+
+    console.log(firstName + " born in " + yearOfBirth
+    + ", is now officially allowed to drive a car. ES6.");
+}
+
+driversLicense6(true);
+
+let i = 23;
+
+for (let i = 0; i < 6; i++) {
+    console.log(i);
+}
+
+console.log(i);
